8d39d8eed65c9b85bc262ae69793666635108d41
Bruno Peres <bruno_16515@hotmail.com>
2023-03-24 11:18:19 -0300

    ✨ Validação de email e senha

--- Desafio#11/app.js
+++ Desafio#11/app.js
@@ -1,6 +1,20 @@
+const form = document.querySelector('form');
 const eye = document.querySelectorAll('.eye');
+const emailError = document.querySelector('.email-error');
+const passwordError = document.querySelector('.password-error');
 
-function togglePassword() {
+const users = [
+  {
+    email: 'bpsilva.dev@gmail.com',
+    password: 'sample123',
+  },
+  {
+    email: 'juju.dev@gmail.com',
+    password: 'sample321',
+  },
+];
+
+function toggleShowPassword() {
   eye.forEach(e => e.classList.toggle('hide'));
 
   const type =
@@ -9,4 +23,51 @@ function togglePassword() {
   password.setAttribute('type', type);
 }
 
-/* Criar validação do email e senha */
+function removeError() {
+  emailError.classList.add('hide');
+  form.email.classList.remove('input-error');
+}
+
+function addError() {
+  emailError.classList.remove('hide');
+  form.email.classList.add('input-error');
+}
+
+function isEmailValid(e) {
+  const emailRegex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]/i;
+
+  if (e.target.value.length === 0) {
+    removeError();
+  } else if (!emailRegex.test(e.target.value)) {
+    addError();
+    emailError.innerText = 'Insira um email valido.';
+  } else {
+    removeError();
+  }
+}
+
+function submitForm(e) {
+  e.preventDefault();
+
+  let isUserValid = false;
+
+  users.forEach(user => {
+    const isEmailCorrect = user.email === form.email.value;
+    const isPasswordCorrect = user.password === form.password.value;
+
+    if (isEmailCorrect && isPasswordCorrect) {
+      isUserValid = true;
+      console.log('Logado com sucesso!');
+    }
+  });
+
+  if (!isUserValid) {
+    passwordError.innerText = 'E-mail ou senha incorretos.';
+    passwordError.classList.remove('hide');
+  } else {
+    passwordError.classList.add('hide');
+  }
+}
+
+form.email.addEventListener('keyup', e => isEmailValid(e));
+form.addEventListener('submit', e => submitForm(e));
