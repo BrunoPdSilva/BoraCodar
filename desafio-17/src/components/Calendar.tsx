270103a247a252dd4f6b1ce8b03372a4b81b5e1f
Bruno Peres <bruno_16515@hotmail.com>
2023-04-27 11:46:11 -0300

    :lipstick: Adding utility classes

--- desafio-17/src/components/Calendar.tsx
+++ desafio-17/src/components/Calendar.tsx
@@ -10,6 +10,51 @@ export function Calendar() {
         <li>Sex</li>
         <li>Sáb</li>
       </ul>
+
+      <ul className="days">
+        <li className="disabled">1</li>
+        <li className="disabled">2</li>
+        <li className="disabled">3</li>
+        <li className="disabled">4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li className="teste">1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li className="today">4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li className="selected first">5</li>
+        <li className="selected">6</li>
+        <li className="selected">7</li>
+        <li className="selected">1</li>
+        <li className="selected">2</li>
+        <li className="selected">3</li>
+        <li className="selected">4</li>
+        <li className="selected last">5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+      </ul>
     </div>
   )
 }
