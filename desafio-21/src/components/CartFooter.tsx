4a2b98aa958871cf059194d1004c94166fc141ca
Bruno Peres <bruno_16515@hotmail.com>
2023-05-25 16:33:11 -0300

    :sparkles: Adicionando input de cupom e efeitos

--- desafio-21/src/components/CartFooter.tsx
+++ desafio-21/src/components/CartFooter.tsx
@@ -1,6 +1,18 @@
-import { Tag } from 'phosphor-react';
+import { Tag, X } from 'phosphor-react';
+import { useState } from 'react';
 
 export function CartFooter() {
+  const [cupom, setCupom] = useState('');
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+
+  const handleFocus = () => {
+    setShowPlaceholder(false);
+  };
+
+  const handleBlur = () => {
+    setShowPlaceholder(true);
+  };
+
   return (
     <footer>
       <div className="price">
@@ -8,10 +20,25 @@ export function CartFooter() {
           <h3>Total: </h3>
           <h2>R$ 10.681,60</h2>
         </div>
-        <button>
+        <label>
           <Tag size={24} />
-          Adicionar cupom
-        </button>
+          <input
+            type="text"
+            placeholder={showPlaceholder ? 'Adicionar cupom' : ''}
+            onChange={e => setCupom(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            value={cupom}
+          />
+
+          <X
+            size={24}
+            color="var(--close-button-color)"
+            className="clearInput"
+            style={{ display: cupom ? 'block' : 'none' }}
+            onClick={() => setCupom('')}
+          />
+        </label>
       </div>
       <button className="cta">Finalizar compra</button>
     </footer>
