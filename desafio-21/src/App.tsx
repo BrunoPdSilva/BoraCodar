334760b13700f023b81c64b7d1c0772397878c4a
Bruno Peres <bruno_16515@hotmail.com>
2023-05-26 13:31:39 -0300

    :rocket: Desafio 21 finalizado

--- desafio-21/src/App.tsx
+++ desafio-21/src/App.tsx
@@ -1,19 +1,53 @@
+import { ShoppingCart } from 'phosphor-react';
 import { CartContextP } from './context/CartContextP';
 import { CartProducts } from './components/CartProducts';
 import { CartHeader } from './components/CartHeader';
 import { CartFooter } from './components/CartFooter';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [showCart, setShowCart] = useState(true);
+
+  const cartVariants = {
+    hidden: {
+      x: '100%',
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <>
       <main></main>
-      <aside>
-        <CartHeader />
-        <CartContextP>
-          <CartProducts />
-          <CartFooter />
-        </CartContextP>
-      </aside>
+
+      <AnimatePresence>
+        {showCart && (
+          <motion.aside
+            className="open"
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={cartVariants}
+            transition={{ duration: 0.3 }}
+          >
+            <CartHeader setShowCart={setShowCart} />
+            <CartContextP>
+              <CartProducts />
+              <CartFooter />
+            </CartContextP>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+
+      {!showCart && (
+        <button className="cart-icon" onClick={() => setShowCart(true)}>
+          <ShoppingCart size={32} />
+        </button>
+      )}
     </>
   );
 }
--- desafio-21/src/components/CartHeader.tsx
+++ desafio-21/src/components/CartHeader.tsx
@@ -1,13 +1,18 @@
+import { Dispatch, SetStateAction } from 'react';
 import { X } from 'phosphor-react';
 import { productsData } from '../data/productsData';
 
-export function CartHeader() {
+interface Props {
+  setShowCart: Dispatch<SetStateAction<boolean>>;
+}
+
+export function CartHeader({ setShowCart }: Props) {
   return (
     <header>
       <h3>
         Seu carrinho tem <b>{productsData.length} itens</b>
       </h3>
-      <button>
+      <button onClick={() => setShowCart(false)}>
         <X size={24} />
       </button>
     </header>
