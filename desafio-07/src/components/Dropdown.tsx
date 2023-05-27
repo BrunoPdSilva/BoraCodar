2db9908499c7732a715431f18f04f44414ba2015
Bruno Peres <bruno_16515@hotmail.com>
2023-05-27 17:30:47 -0300

    :sparkles: Header finalizado.
    - Form de pesquisa:
    -- Input para pesquisa por nome
    -- Dropdown para selecionar uma cidade disponível.

---
+++ desafio-07/src/components/Dropdown.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  handleSelect: (value: string) => void;
+}
+
+export function Dropdown({ handleSelect }: Props) {
+  return (
+    <ul className="dropdown">
+      <li onClick={() => handleSelect('')}>Mostrar todas</li>
+      <li onClick={() => handleSelect('Curitiba')}>Curitiba</li>
+      <li onClick={() => handleSelect('Florianópolis')}>Florianópolis</li>
+      <li onClick={() => handleSelect('Porto Alegre')}>Porto Alegre</li>
+      <li onClick={() => handleSelect('Rio de Janeiro')}>Rio de Janeiro</li>
+      <li onClick={() => handleSelect('São Paulo')}>São Paulo</li>
+    </ul>
+  );
+}
---
+++ desafio-07/src/components/Header.tsx
@@ -0,0 +1,68 @@
+import { CaretDown, MagnifyingGlass, MapPin } from 'phosphor-react';
+import { useEffect, useRef, useState } from 'react';
+import { Dropdown } from './Dropdown';
+
+import topIllustration from '../assets/ilustra-01.svg';
+import bottomIllustration from '../assets/ilustra-02.svg';
+
+export function Header() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [dropdownValue, setDropdownValue] = useState('');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const ref = dropdownRef.current;
+
+      if (ref && !ref.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  function handleSelect(value: string) {
+    setDropdownValue(value);
+    setShowDropdown(false);
+  }
+
+  return (
+    <>
+      <header>
+        <img src={topIllustration} alt="Top Illustration" />
+
+        <section>
+          <div className="heading">
+            <p>Find your block</p>
+            <h1>Encontre os <span>melhores blocos</span> de carnaval de 2023</h1>
+          </div>
+
+          <form name="search">
+            <div className="input-group">
+              <MagnifyingGlass size={24} color="#E45858" />
+              <input type="text" placeholder="Pesquise por nome" />
+            </div>
+
+            <div className="input-group" ref={dropdownRef} onClick={() => setShowDropdown(!showDropdown)}>
+              <MapPin size={24} color="#E45858" />
+              <p>{dropdownValue || 'Selecione uma cidade'}</p>
+              <button type="button"              >
+                <CaretDown size={24} color="#858793" />
+              </button>
+
+              {showDropdown && <Dropdown handleSelect={handleSelect} />}
+            </div>
+
+            <button type="submit">BUSCAR AGORA</button>
+          </form>
+        </section>
+
+        <img src={bottomIllustration} alt="Bottom Illustration" />
+      </header>
+    </>
+  );
+}
