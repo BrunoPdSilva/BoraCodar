a464d16bfeb550a9ff68795e1e7c52387566bb4d
Bruno Peres <bruno_16515@hotmail.com>
2023-06-15 08:59:36 -0300

    :lipstick: Estilização base

--- desafio-24/src/App.tsx
+++ desafio-24/src/App.tsx
@@ -1,7 +1,29 @@
+import { Palette, Sun, CircleHalf } from "phosphor-react";
+
 function App() {
 
   return (
     <div id="app">
+      <div className="circle-container">
+        <div className="circle"></div>
+      </div>
+
+      <h1>Ajustes de iluminação</h1>
+
+      <section className="controls">
+        <div>
+          <Palette size={24} color="#ffffff" />
+          <input type="range" min={0} max={360} />
+        </div>
+        <div>
+          <Sun size={24} color="#ffffff" />
+          <input type="range" min={0} max={100} />
+        </div>
+        <div>
+          <CircleHalf size={24} color="#ffffff" />
+          <input type="range" min={0} max={100} />
+        </div>
+      </section>
     </div>
   )
 }
