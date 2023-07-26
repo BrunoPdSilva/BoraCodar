5a44bc912250b1e9a3871665cf1b05e287bd55f9
Bruno Peres <bruno_16515@hotmail.com>
2023-07-26 15:36:13 -0300

    :tada: Desafio 30 - Primeiro commit

---
+++ desafio-30/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="h-screen bg-gradient2 flex items-center justify-center">
+      <main></main>
+    </div>
+  );
+}
---
+++ desafio-30/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      purple: "#8323FF",
+      pink: "##FF2DAF",
+      yellow: "##FEEA35",
+      white: "#FFFFFF",
+      gray: {
+        100: "#373745",
+        200: "#2B2B37",
+        300: "#1E1F28",
+      },
+    },
+    backgroundImage: {
+      gradient1: "linear-gradient(90deg, #8323FF 0%, #FF2DAF 100%)",
+      gradient2: "linear-gradient(90deg, #9F55FF 0%, #FF46B9 100%)",
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
