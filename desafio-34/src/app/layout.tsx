9b690c09aecb01e9906a2e93e1046cb48830c773
Bruno Peres <bruno_16515@hotmail.com>
2023-08-24 11:18:40 -0300

    :tada: Initial commit

---
+++ desafio-34/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Desafio 34 - Lista de compras",
+  description:
+    "Desafio 34 do BoraCodar da Rocketseat, o desafio consiste em uma lista de compras.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ desafio-34/tailwind.config.ts
@@ -0,0 +1,43 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      gray: {
+        100: "#FBF9FE",
+        200: "#AFABB6",
+        300: "#252529",
+        400: "#17171A",
+        500: "#111112",
+        600: "#0C0C0D",
+      },
+      purple: {
+        100: "#A881E6",
+        200: "#7450AC",
+        300: "#523480",
+      },
+      green: {
+        100: "#4E995E",
+        200: "#2F723D",
+      },
+      pink: "#DB5BBF",
+      pink_dark: "#251622",
+      orange: "#E07B67",
+      orange_dark: "#261A17",
+      yellow: "#BB9F3A",
+      yellow_dark: "#211E12",
+      blue: "#7B94CB",
+      blue_dark: "#1A1D23",
+      second_green: "#8CAD51",
+      second_green_dark: "#1C2015",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
